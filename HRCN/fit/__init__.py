88d527866b3aad14579adb01e895373f1879c5f5
YuanzhengMa <76952737+YuanzhengMa@users.noreply.github.com>
2022-03-25 10:11:17 +0800

    Delete HRCN directory

--- HRCN/fit/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: UTF-8 -*-
-# @Start-time      : 2021/6/24 18:13
-# @File-name       : __init__.py.py
-# @Description     :
--- HRCN/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: UTF-8 -*-
-# @Start-time      : 2021/6/21 10:15
-# @File-name       : __init__.py.py
-# @Description     :
